ff5bf0b423992a0bc868a0bb3d216386580f6751
Jenifer Placido <jenifer.sdti@gmail.com>
2024-09-09 21:36:06 -0300

    Aula 01

---
+++ Aulas/Aula01/aula.js
@@ -0,0 +1,20 @@
+// Tipos de Dados em Javascript
+
+let numero = 10;   // Number
+let texto = 'Jenifer'; // String
+let booleano = true; // Boolean
+let indefinido = undefined; // Undefined
+let nulo = null; // Null
+let objeto = {    // Object
+    nome:'Jenifer',
+    idade: 34,
+    profissao: 'Programadora'
+};
+let array = [1,2,3,4,5]; // Array
+
+var variavelGlobal = 'Global';
+let variavelLocal = 'Local';
+const semAlterar = 3.14
+
+console.log(variavelGlobal, variavelLocal, semAlterar)
+console.log(numero, texto, booleano, indefinido, nulo, objeto, array)
---
+++ Aulas/Aula01/console.js
@@ -0,0 +1,4 @@
+console.log("Olá Coders") // para mostrar algo no console
+console.warn("Hello") // para verificar alertas no navegador
+console.error('Hello')// para verificar erros no navegador
+console.clear() // para limpar o console
